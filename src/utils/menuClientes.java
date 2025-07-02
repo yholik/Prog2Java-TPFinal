401fb3440a47de887bda2dc603621e33ccb66b86
Biosidus <emilianoam167@gmail.com>
2025-07-02 15:21:47 -0300

    Arreglo del metodo de registrar cliente y creacion del metodo accesoCliente, se deberia crear metodo en UsuarioContainer para el login o modificar el metodo getUser

--- src/utils/menuClientes.java
+++ src/utils/menuClientes.java
@@ -5,76 +5,100 @@ import models.Usuario;
 import java.util.ArrayList;
 import containers.UsuarioContainer;
 
-
 public class menuClientes {
 
 	private Usuario user;
 	private Scanner sc;
 	private UsuarioContainer containerUser;
-	
+
 	public menuClientes(UsuarioContainer containerUser, Scanner sc) {
 		this.containerUser = containerUser;
 		this.sc = sc;
 	}
-	
-	
-	//Mostrar el menu con las opciones y las acciones que puede trabajar
+
+	// Mostrar el menu con las opciones y las acciones que puede trabajar
 	public void mostrarMenu() {
 		boolean continuar = true;
 		System.out.println("¡Bienvenido al menu de Clientes!");
-		while(continuar) {
+		while (continuar) {
 			System.out.println("Elija una opcion:");
-			System.out.println("1. Ingresar con usuario y cogit ntraseña");
+			System.out.println("1. Ingresar con usuario y contraseña");
 			System.out.println("2. Resgistrarse");
 			System.out.println("3. Salir");
 			int opc = sc.nextInt();
 			switch (opc) {
-            case 1 : accesoCliente();
-            case 2 : agregarNuevoUsuario(containerUser);
-            case 3 : continuar = false; System.out.println("Saliendo...");
-            default : System.out.println("Opción inválida.");
+			case 1:
+				accesoCliente();
+				break;
+			case 2:
+				agregarNuevoUsuario();
+				break;
+			case 3:
+				continuar = false;
+				System.out.println("Saliendo...");
+				break;
+			default:
+				System.out.println("Opción inválida.");
+				break;
+			}
+
 		}
-		
-		
-	}	
-}
-	
+	}
+
+	// Metodo de acceso cliente
 	public void accesoCliente() {
+		System.out.println("Ingrese su nombre de usuario:");
+		String nombreIngresado = sc.next();
+		
+		System.out.println("Ingrese su contraseña:");
+		String contraseñaIngresada = sc.next();
+		
+		//faltaria agregar al lado de nombreIngresado contraseñaIngresada como parametro 
+		Usuario cliente = containerUser.getUser(nombreIngresado);
+		
+		if (cliente != null) {
+			System.out.println("Acceso permitido. Bienvenido " + cliente.getNombreUser()+"!");
+			//aca se deberia incluir un acceso al menu de funciones del usuario logeado
+			
+		}else {
+			System.out.println("Nombre de usuario o contraseña incorrectos.");
+		}
 		
 	}
-	
-	public void agregarNuevoUsuario(UsuarioContainer containerUser) {		
+
+	// Metodo de agregar nuevo usuario, se verifica que la contraseña se ingrese
+	// bien 2 veces
+	public void agregarNuevoUsuario() {
 		System.out.println("Ingrese el nombre del usuario nuevo");
 		String nombreNuevo = sc.next();
-		
-		//Verificar si ese nombre existe
-		Usuario usuarioBuscado = containerUser.getUser(nombreNuevo);
-		//TODA LA VALIDACION ES INNECESARIA XQ PUEDEN REPETIRSE LOS USUARIOS
-		if(usuarioBuscado != null) {
-			System.out.println("El usuario ya existe!");
-		}else {
-			boolean listo = false;
-			String nPassword = "";
-			do {
-				System.out.println("Ingrese la contraseña:");
-				nPassword = sc.next();
-				
-				if(!nPassword.isEmpty()) {
-					listo = true;
-				}else {
-					System.out.println("La contraseña no puede estar vaica.");
-				}
-			}while(!listo);
-			
-			Usuario nUsuario = new Usuario(nombreNuevo, nPassword);
-			containerUser.agregarUsuario(nUsuario);
-			
-			System.out.println("El usuario se registro correctamente.");
-		}
-		
-		
-		
-		
+
+		boolean listo = false;
+		String nPassword = "";// La inicializo por las dudas
+		String confirmacionPassword = "";
+
+		do {
+			// Ingresa por primera vez la contraseña
+			System.out.println("Ingrese la contraseña:");
+			nPassword = sc.next();
+
+			// Reingreso para verificar
+			System.out.println("Confirme la contraseña:");
+			confirmacionPassword = sc.next();
+
+			if (nPassword.isEmpty() || confirmacionPassword.isEmpty()) {
+				System.out.println("La contraseña no puede estar vacia.");
+			} else if (!nPassword.equals(confirmacionPassword)) {
+				System.out.println("Las contraseñas no coinciden. Intente nuevamente.");
+			} else {
+				System.out.println("Las contraseñas coinciden.");
+				listo = true;
+			}
+		} while (!listo);
+
+		Usuario nUsuario = new Usuario(nombreNuevo, nPassword);
+		containerUser.agregarUsuario(nUsuario);
+
+		System.out.println("El usuario se registro correctamente.");
 	}
 
 }
