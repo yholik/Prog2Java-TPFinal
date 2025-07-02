8c0ff22496f231a10784f12eb7fc428ee0b36546
Biosidus <emilianoam167@gmail.com>
2025-07-01 22:03:22 -0300

    Creacion de packeage UTILS y clase menuClientes(sin terminar, tiene solo el menu visual y recien empezado la opcion de registro de usuarios.)

---
+++ src/utils/menuClientes.java
@@ -0,0 +1,58 @@
+package utils;
+
+import java.util.Scanner;
+import models.Usuario;
+import java.util.ArrayList;
+import containers.UsuarioContainer;
+
+
+public class menuClientes {
+
+	private Scanner sc;
+	private UsuarioContainer containerUser;
+	
+	public menuClientes(UsuarioContainer containerUser, Scanner sc) {
+		this.containerUser = containerUser;
+		this.sc = sc;
+	}
+	
+	
+	//Mostrar el menu con las opciones y las acciones que puede trabajar
+	public void mostrarMenu() {
+		boolean continuar = true;
+		System.out.println("¡Bienvenido al menu de Clientes!");
+		while(continuar) {
+			System.out.println("Elija una opcion:");
+			System.out.println("1. Ingresar con usuario y cogit ntraseña");
+			System.out.println("2. Resgistrarse");
+			System.out.println("3. Salir");
+			int opc = sc.nextInt();
+			switch (opc) {
+            case 1 : accesoCliente();
+            case 2 : agregarNuevoUsuario();
+            case 3 : continuar = false; System.out.println("Saliendo...");
+            default : System.out.println("Opción inválida.");
+		}
+		
+		
+	}	
+}
+	
+	public void accesoCliente() {
+		
+	}
+	
+	public void agregarNuevoUsuario() {
+		
+		/*System.out.println("Ingrese el nombre del usuario nuevo");
+		do {
+			String nombreNuevo = sc.next();
+			
+		}while;*/
+		
+		
+		
+		
+	}
+
+}
